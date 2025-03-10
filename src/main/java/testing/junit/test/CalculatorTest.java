8429a068d0c87169c99107d23cc4b33c28897de8
Shivkant Kaushik <shivkantkaushik@Shivkants-MacBook-Air.local>
2025-03-10 21:21:28 +0530

    Test Fixtures

--- src/main/java/testing/junit/test/CalculatorTest.java
+++ src/main/java/testing/junit/test/CalculatorTest.java
@@ -1,7 +1,6 @@
 package testing.junit.test;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import testing.junit.classes.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,10 +10,41 @@ public class CalculatorTest {
 
     Calculator calculator = new Calculator();
 
+    int x;
+    int y;
+
+    @BeforeAll
+    public static void setUp(){
+        System.out.println("It will run only once, before all functions.");
+    }
+
+    @BeforeEach
+    public void initializeVariables(TestInfo testInfo){
+
+        System.out.println("Method Name: " + testInfo.getTestMethod());
+        System.out.println("Display Name: " + testInfo.getDisplayName());
+
+        x = 10;
+        y = 10;
+    }
+
+    @AfterEach
+    public void clearVariables(){
+        x = 0;
+        y = 0;
+    }
+
     @Test
     @DisplayName("Addition Method")
     public void testAddition(){
-        assertEquals(21,calculator.add(10,10));
+        assertEquals(20,calculator.add(x,y));
+    }
+
+
+
+    @AfterAll
+    public static void deSetup(){
+        System.out.println("It will run once after all teh functions.");
     }
 
 
