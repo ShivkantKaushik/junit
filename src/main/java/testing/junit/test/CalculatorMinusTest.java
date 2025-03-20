5874a562b90a42b56e527ba3a6fa51fe64c16530
Shivkant Kaushik <shivkantkaushik@Shivkants-MacBook-Air.local>
2025-03-20 23:45:51 +0530

    Parameterized Test with CsvSource

--- src/main/java/testing/junit/test/CalculatorMinusTest.java
+++ src/main/java/testing/junit/test/CalculatorMinusTest.java
@@ -3,6 +3,9 @@ package testing.junit.test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import testing.junit.classes.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,4 +28,20 @@ public class CalculatorMinusTest {
 
         assertEquals(8, calculator.subtract(x , y));
     }
+
+
+    @ParameterizedTest
+    @CsvSource({"8,10,2" , "12,15,3"})
+    @DisplayName("Parameterized Minus Test | Csv Source")
+    void minusTest(int res, int a, int b){
+
+        assertEquals(res,calculator.subtract(a , b));
+
+    }
+
+
+
+
+
+
 }
