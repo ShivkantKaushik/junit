8012540e08d7089615127fa4c71b0e19258e5feb
Shivkant Kaushik <shivkantkaushik@Shivkants-MacBook-Air.local>
2025-03-21 13:53:47 +0530

    Parameterized Tests, others

--- src/main/java/testing/junit/test/CalculatorMinusTest.java
+++ src/main/java/testing/junit/test/CalculatorMinusTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import testing.junit.classes.Calculator;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -38,6 +43,37 @@ public class CalculatorMinusTest {
         assertEquals(res,calculator.subtract(a , b));
 
     }
+
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1})
+    @DisplayName("Parametrized Value Source")
+    void minusTest1(int argument){
+
+        assertEquals(9, calculator.subtract(10, argument));
+
+    }
+
+
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    @DisplayName("Parameterized, Method Source")
+    void minusTest2(int res, int a, int b){
+        assertEquals(res, calculator.subtract(a,b));
+    }
+
+
+   static Stream<Arguments> provider(){
+
+
+        return Stream.of( Arguments.of(1,10,9) );
+    }
+
+
+
+
 
 
 
